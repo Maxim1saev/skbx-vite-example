462f1790b8c2145b2dcf9b4223468ebe0301e0de
Maxim1saev <37245038+Maxim1saev@users.noreply.github.com>
2024-12-22 23:21:41 +0300

    11111

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import "./App.css";
 
-// import { InvisibleSmartCaptcha } from "@yandex/smart-captcha";
-// import GoogleRecaptcha from "react-google-invisible-recaptcha";
+import GoogleRecaptcha from "react-google-invisible-recaptcha";
 function App() {
   const [value, setValue] = useState("");
   const [resolved, setResolved] = useState(false);
 
-  // const refRecaptcha = useRef(null);
+  const refRecaptcha = useRef(null);
 
-  // const onResolved = () => {
-  //   // alert(
-  //   //   name +
-  //   //     ": Recaptcha resolved with response: " +
-  //   //     refRecaptcha.current.callbacks.getResponse()
-  //   // );
-  //   setResolved(true);
-  // };
+  const onResolved = () => {
+    const data = refRecaptcha.current.callbacks.getResponse();
+    refRecaptcha.current.callbacks.reset();
+
+    console.log("data", data);
+    setResolved(true);
+  };
 
   const onRestart = () => {
     setValue("");
@@ -26,10 +24,7 @@ function App() {
 
   const onSubmit = () => {
     // refRecaptcha.current.callbacks.reset();
-    // refRecaptcha.current.callbacks.execute();
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    if (window.smartCaptcha) window.smartCaptcha.execute(0);
+    refRecaptcha.current.callbacks.execute();
   };
 
   return resolved ? (
@@ -45,11 +40,11 @@ function App() {
         onChange={(event) => setValue(event.target.value)}
       />
       <button onClick={onSubmit}>Submit</button>
-      {/* <GoogleRecaptcha
+      <GoogleRecaptcha
         onResolved={onResolved}
         ref={refRecaptcha}
         sitekey="6LeH_x8UAAAAAKKuaaod4GsENkTJTHdeQIm8l6y2"
-      /> */}
+      />
     </div>
   );
 }
