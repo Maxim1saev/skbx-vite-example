6d5d6c3dc1303cb05a7534d02563dc520a52391a
Maxim1saev <37245038+Maxim1saev@users.noreply.github.com>
2024-12-23 00:22:24 +0300

    yyy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { useRef, useState } from "react";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
 
+import { useRef, useState } from "react";
 import "./App.css";
 
 import GoogleRecaptcha from "react-google-invisible-recaptcha";
@@ -23,29 +25,33 @@ function App() {
   };
 
   const onSubmit = () => {
-    // refRecaptcha.current.callbacks.reset();
+    refRecaptcha.current.callbacks.reset();
     refRecaptcha.current.callbacks.execute();
   };
 
-  return resolved ? (
-    <div>
-      Human detected!
-      <button onClick={onRestart}>Restart</button>
-    </div>
-  ) : (
-    <div>
-      <input
-        type="text"
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-      />
-      <button onClick={onSubmit}>Submit</button>
-      <GoogleRecaptcha
-        onResolved={onResolved}
-        ref={refRecaptcha}
-        sitekey="6LeH_x8UAAAAAKKuaaod4GsENkTJTHdeQIm8l6y2"
-      />
-    </div>
+  return (
+    <>
+      {resolved ? (
+        <div>
+          Human detected!
+          <button onClick={onRestart}>Restart</button>
+        </div>
+      ) : (
+        <div>
+          <input
+            type="text"
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          />
+          <button onClick={onSubmit}>Submit</button>
+          <GoogleRecaptcha
+            onResolved={onResolved}
+            ref={refRecaptcha}
+            sitekey="6LeH_x8UAAAAAKKuaaod4GsENkTJTHdeQIm8l6y2"
+          />
+        </div>
+      )}
+    </>
   );
 }
 
